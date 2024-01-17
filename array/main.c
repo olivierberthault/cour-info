bacdb8a1cfcf0994f9d378ea8b97f2f87e57ed5e
olivierberthault <olivberthault@gmail.com>
2024-01-17 15:03:53 +0100

    array

---
+++ array/main.c
@@ -0,0 +1,18 @@
+int main(void)
+{
+	int array[6] = {312,354,314,3154,-6987,514};
+	return 0;
+}
+int get_biggest(int* ptr, int size)
+{
+	int i;
+	int biggest;
+	
+	i = 0;
+	biggest = INT_MIN;
+
+	while(i=-6)
+	{
+		i++
+	}
+}
