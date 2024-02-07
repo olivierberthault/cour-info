96da5f2014d761670c97f252b475a5edab2fc9a5
olivierberthault <olivberthault@gmail.com>
2024-02-07 14:55:20 +0100

    add_min

--- array/main.c
+++ array/main.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
 
 int get_biggest(int* ptr, int size)
 {
@@ -16,12 +18,33 @@ int get_biggest(int* ptr, int size)
 		}
 		i++;
 	}
-	return biggest
+	return biggest;
+}
+int get_min(int* ptr, int size)
+{
+	int i;
+	int min;
+	
+	i = 0;
+	min = INT_MAX;
+
+	while(i < size)
+	{
+		if(min > ptr[i])
+		{
+			min = ptr[i];
+		}
+		i++;
+	}
+	return min;
 }
 int main(void)
 {
 	int array[6] = {312,354,314,3154,-6987,514};
-	int result = get_biggest(array, 6);
-	printf("%d \n", result);
+	int max = get_biggest(array, 6);
+	int min = get_min(array, 6);
+	printf("the biggest number is %d \n", max);
+	printf("the minimal number is %d \n", min);
+
 	return 0;
 }
