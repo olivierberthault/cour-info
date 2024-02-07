0ffc3fa427e78f6f7f1a709fe54c94e657e9d793
olivierberthault <olivberthault@gmail.com>
2024-02-07 14:35:00 +0100

    array

--- array/main.c
+++ array/main.c
@@ -1,8 +1,5 @@
-int main(void)
-{
-	int array[6] = {312,354,314,3154,-6987,514};
-	return 0;
-}
+#include <stdlib.h>
+
 int get_biggest(int* ptr, int size)
 {
 	int i;
@@ -11,8 +8,20 @@ int get_biggest(int* ptr, int size)
 	i = 0;
 	biggest = INT_MIN;
 
-	while(i=-6)
+	while(i < size)
 	{
-		i++
+		if(biggest < ptr[i])
+		{
+			biggest = ptr[i];
+		}
+		i++;
 	}
+	return biggest
+}
+int main(void)
+{
+	int array[6] = {312,354,314,3154,-6987,514};
+	int result = get_biggest(array, 6);
+	printf("%d \n", result);
+	return 0;
 }
