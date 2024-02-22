ee8c263d6853077a5707c2b3e94de7dd1ecf6c1e
olivierberthault <olivberthault@gmail.com>
2024-02-22 14:15:07 +0100

    add_main_arguments

--- main.c
+++ main.c
@@ -7,34 +7,12 @@ void print_sign(long nb)
 		printf("%ld est un nombre nul \n",nb);
 	else if (nb < 0)
 		printf("%ld est un nombre negatif \n",nb);
+	nb = 0;
 } 
-int main(void)
+int main(int ac, char**av)
 {
-	print_sign(112433);
-	print_sign(-112433);
-	print_sign(0);
-	print_sign(11152433);
-	print_sign(-112433);
-	print_sign(-112433);
-	print_sign(-112433);
-	print_sign(11712433);
-	print_sign(11412433);
-	print_sign(112147433);
-	print_sign(1124121174133);
-	print_sign(11522433);
-	print_sign(112433);
-	print_sign(11233);
-	print_sign(1182782433);
-	print_sign(1122551243);
-	print_sign(11175175133);
-	print_sign(112417533);
-	print_sign(112781433);
-	print_sign(1122433);
-	print_sign(1121871272433);
-	print_sign(152185172433);
-	print_sign(117582433);
-	print_sign(11171282433);
-	print_sign(11781433);
-	print_sign(11822433);
-	return 0;
+	int i = 0;
+	while(i < ac):x
+
+	return 0
 }
