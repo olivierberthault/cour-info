22edbc0852ccb5109273d72f0d37f41207780964
olivierberthault <olivberthault@gmail.com>
2024-07-29 14:30:04 +0200

    user can interact with program

--- time.c
+++ time.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct s_time
 {
@@ -9,10 +10,10 @@ struct s_time
 
 void print_time(struct s_time duration)
 {
-	printf("%lu : %lu : %lu \n", duration.hour, duration.minute, duration.second);
+	printf("%lu h : %lu m : %lu s \n", duration.hour, duration.minute, duration.second);
 }
 
-struct s_time convert_second_into_duration(unsigned long second_count)
+struct s_time convert_second_into_duration(const unsigned long second_count)
 {
 	struct s_time duration;
 	duration.hour = second_count / 3600;
@@ -21,11 +22,19 @@ struct s_time convert_second_into_duration(unsigned long second_count)
 	return duration;
 }
 
-int main()
+int main(int ac, char** av)
 {
-	struct s_time duration;
-	unsigned long time_input = 12541616416;
-	duration = convert_second_into_duration(time_input);
-	print_time(duration);
-	return 0;
+	if(ac != 2)
+	{
+		printf("Il ne faut qu'un argument : ./time <second_count> \n");
+		return 1;
+	}
+	else
+	{
+		struct s_time duration;
+		const unsigned long time_input = strtoul(av[1], av + 1, 10);
+		duration = convert_second_into_duration(time_input);
+		print_time(duration);
+		return 0;
+	}
 }
